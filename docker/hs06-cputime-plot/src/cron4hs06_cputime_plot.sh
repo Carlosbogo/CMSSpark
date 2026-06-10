20ae0bf046ee3f7ff9a2e861190fd63b9a0eed3b
carlosbogo <carlos.borrajo.gomez@cern.ch>
2026-06-10 15:32:06 +0200

    Add otel support for logs

--- docker/hs06-cputime-plot/src/cron4hs06_cputime_plot.sh
+++ docker/hs06-cputime-plot/src/cron4hs06_cputime_plot.sh
@@ -52,7 +52,7 @@ spark_submit_args=(
 
 # run spark function
 function run_spark() {
-    spark-submit "${spark_submit_args[@]}" "$script_dir/condor_hs06coreHrPlot.py" "$@"
+    util_spark_submit_with_otel_logs "${spark_submit_args[@]}" "$script_dir/condor_hs06coreHrPlot.py" "$@"
 }
 
 END_DATE="$(date +%Y-%m-01)"
