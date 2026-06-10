b682cccd117e7cd67b076f00f9591fbcb6aeb6fd
carlosbogo <carlos.borrajo.gomez@cern.ch>
2026-06-10 15:27:26 +0200

    Add otel support for logs

--- docker/eos-dataset/src/cron4eos_dataset.sh
+++ docker/eos-dataset/src/cron4eos_dataset.sh
@@ -50,7 +50,7 @@ spark_submit_args=(
 
 # run spark function
 function run_spark() {
-    spark-submit "${spark_submit_args[@]}" "$script_dir/dbs_hdfs_eos.py" $@
+    util_spark_submit_with_otel_logs "${spark_submit_args[@]}" "$script_dir/dbs_hdfs_eos.py" $@
 }
 
 # Generate the previous month intermediate files
